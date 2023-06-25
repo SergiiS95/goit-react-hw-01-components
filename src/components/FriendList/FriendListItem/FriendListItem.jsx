180d35aba69d85d895b6558613dcd31c0ce3e22e
SergiiS95 <SSirota1995@gmail.com>
2023-06-25 11:03:15 +0300

    update hw1

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -2,24 +2,9 @@ import PropTypes from 'prop-types';
 import css from './FriendListItem.module.css';
 
 export const FriendListItem = ({ avatar, name, isOnline }) => {
- 
-  function getBgColor(isOnline) {
-    if (isOnline) {
-    return "green"
-    }
-    else {
-      return "red"
-    }
-}
-
   return (
     <li className={css.item}>
-      <span
-        className={css.status}
-        style={{ backgroundColor: getBgColor(isOnline) }}
-      >
-        {isOnline}
-      </span>
+      <span className={isOnline ? css.online : css.offline} />
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p className={css.name}>{name}</p>
     </li>
